1f84cbdd22cc73b778d9a9b5fa4c910897cba6e8
Sylvia Boonabaana <sylviaboona2@gmail.com>
2021-04-06 23:50:21 +0300

    edited App component to removed unusued componrnts

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,39 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello';
-import Message from './components/Message'
-import Counter from './components/Counter'
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ChildComponent from './components/ChildComponent';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Person from './components/Person';
-import StyleSheet from './components/StyleSheet';
-import Inline from './components/Inline';
+// import Greet from './components/Greet'
+// import Welcome from './components/Welcome'
+// import Hello from './components/Hello';
+// import Message from './components/Message'
+// import Counter from './components/Counter'
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+// import ChildComponent from './components/ChildComponent';
+// import ParentComponent from './components/ParentComponent';
+// import UserGreeting from './components/UserGreeting';
+// import NameList from './components/NameList';
+// import Person from './components/Person';
+// import StyleSheet from './components/StyleSheet';
+// import Inline from './components/Inline';
 import './appStyles.css';
-import styles from './appStyles.module.css'
-import Form from './components/Form';
-import LifeCycleA from './components/LifeCycleA';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-import PureComp from './components/PureComp';
-import ParentComp from './components/ParentComp';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput';
-import FWDRefParentInput from './components/FWDRefParentInput';
-import PortalDemo from './components/PortalDemo';
-import Hero from './components/Hero';
-import ErrorBoundary from './components/ErrorBoundary';
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
+// import styles from './appStyles.module.css'
+// import Form from './components/Form';
+// import LifeCycleA from './components/LifeCycleA';
+// import FragmentDemo from './components/FragmentDemo';
+// import Table from './components/Table';
+// import PureComp from './components/PureComp';
+// import ParentComp from './components/ParentComp';
+// import RefsDemo from './components/RefsDemo';
+// import FocusInput from './components/FocusInput';
+// import FWDRefParentInput from './components/FWDRefParentInput';
+// import PortalDemo from './components/PortalDemo';
+// import Hero from './components/Hero';
+// import ErrorBoundary from './components/ErrorBoundary';
+// import ClickCounter from './components/ClickCounter';
+// import HoverCounter from './components/HoverCounter';
 import ClickCounterTwo from './components/ClickCounterTwo';
 import HoverCounterTwo from './components/HoverCounterTwo';
-import User from './components/User';
+// import User from './components/User';
 import CounterTwo from './components/CounterTwo';
 
 
